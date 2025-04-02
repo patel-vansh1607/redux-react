f36e444f69e718b1697d701472e086d519136caf
Vansh Patel <patel.vansh1607@gmail.com>
2025-04-02 16:59:51 +0300

    import store from the store module in index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM, { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
+import store from "./store"
 
 const container = ReactDOM.createRoot(document.getElementById('root'));
 
