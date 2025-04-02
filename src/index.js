1581ec82c91a01da0d7d137d8abe0cfdfeaa8d30
Vansh Patel <patel.vansh1607@gmail.com>
2025-04-02 17:08:48 +0300

    refactor index.js to streamline imports and improve readability

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
-import React from 'react';
-import ReactDOM, { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { Provider } from 'react-redux';
-import store from "./store"
+import * as React from 'react'
+import { createRoot } from 'react-dom/client'
+import { Provider } from 'react-redux'
 
 const container = ReactDOM.createRoot(document.getElementById('root'));
 
