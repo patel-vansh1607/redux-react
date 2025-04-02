f4e1ef8d75bb4dfba510c23e5c3b7a2c69ef3f44
Vansh Patel <patel.vansh1607@gmail.com>
2025-04-02 16:59:43 +0300

    implement loading and error handling for pokemonApi query in App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
-import pokemonApi from "./services/poki"
+import pokemonApi, { useGetPokemonByNameQuery } from "./services/pokemon"
+import Loader from './components/loader';
+
 
 function App() {
+  const [data, isLoading, isError] = useGetPokemonByNameQuery
+
+  if(isError){
+    return <p>Error Landing</p>
+  } 
+
+  if(isLoading) {
+    return <Loader />
+  }
   return (
     <div className="App">
-      <pokemonApi />
+      <h1>hi</h1>
     </div>
   );
 }
